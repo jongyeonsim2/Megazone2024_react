732d81eaacca3b978d71716fba79ad487db8dae7
unknown <jongyeonsim2@gmail.com>
2024-07-03 13:11:21 +0900

    5st commit

--- javascript/ch2/addText-result.js
+++ javascript/ch2/addText-result.js
@@ -14,11 +14,23 @@ const orderInfo = document.querySelector("#orderInfo");
 orderBtn.addEventListener(
   "click",
   () => {
-    // DOM에 추가할 노드(p tag)를 생성
-    // text node 생성해서 도서 title 을 저장.
-    // p tag와 text node 관계 설정
-    // p tag 스타일 적용
-    // 부모 node(div tag) 와 p tag 를 연결
+    // 브라우저 DOM 에 직접 요소 추가
+    // 1. DOM에 추가할 노드(p tag)를 생성
+    let newP = document.createElement("p");
+
+    // 2. text node 생성해서 도서 title 을 저장.
+    //    container > h2 요소의 요소값(도서 제목)을 가져와서 저장.
+    let textNode = document.createTextNode(title.innerText);
+
+    // 3. p tag와 text node 관계 설정
+    newP.appendChild(textNode);
+
+    // 4. p tag 스타일 적용
+    newP.style.fontSize = "2em"; // 32px
+    newP.style.color = "red";
+
+    // 5. 부모 node(div tag) 와 p tag 를 연결
+    orderInfo.appendChild(newP);
   },
   { once: false }
 );
