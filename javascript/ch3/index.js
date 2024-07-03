67c85d5208bc4bedb702feeda6ad303f4af80382
unknown <jongyeonsim2@gmail.com>
2024-07-03 11:24:44 +0900

    1st commit

--- javascript/ch3/index.js
+++ javascript/ch3/index.js
@@ -1,4 +1,11 @@
 import * as circle from "./circle.js";
+import lodash from "lodash";
+
+const arr = [1111, 222, 111, 44, 33, 222];
+
+const uniueArr = lodash.uniqBy(arr);
+
+console.log(uniueArr);
 
 console.log(circle.PI, circle.getArea(1), circle.getCircumgerence(1));
 
