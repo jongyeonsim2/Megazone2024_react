e2dd7177b6e697325f25377d398d4f8212c609d6
unknown <jongyeonsim2@gmail.com>
2024-07-03 15:12:27 +0900

    2st commit

--- javascript/ch2/async/user-json-from-server.js
+++ javascript/ch2/async/user-json-from-server.js
@@ -1,9 +1,15 @@
 /**
  * 1. 대량의 데이터를 서버에 요청을 가정.
+ *
  * 2. 서버와 통신시 fetch API 사용.( promise 반환되기 때문 )
+ *   서버 요청 주소는 아래의 주소를 사용.
+ *    https://jsonplaceholder.typicode.com/users
+ *
  * 3. fetch API 실행시 await 예약어 사용.
+ *
  * 4. response 되는 것은 JSON임. 객체로 변환.
  *    response.json() 실행시 await 예약어 사용.
+ *
  * 5. 객체 배열을 화면에 출력하기 위한 함수 구현.
  *
  */
