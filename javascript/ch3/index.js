d2a5a19a2fd8c03532b96f128f2d7dc5a2d484bf
unknown <jongyeonsim2@gmail.com>
2024-07-03 11:33:48 +0900

    2st commit

--- javascript/ch3/index.js
+++ javascript/ch3/index.js
@@ -3,9 +3,12 @@ import lodash from "lodash";
 
 const arr = [1111, 222, 111, 44, 33, 222];
 
+// lodash 에는 객체와 배열관련 기능을 제공.
 const uniueArr = lodash.uniqBy(arr);
 
 console.log(uniueArr);
+
+// 터미널에서 npm run start 입력
 
 console.log(circle.PI, circle.getArea(1), circle.getCircumgerence(1));
 
