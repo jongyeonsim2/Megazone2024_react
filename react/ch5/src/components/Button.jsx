5957812f7b1576516fe321cb76fb96c5b0283f80
unknown <jongyeonsim2@gmail.com>
2024-07-04 14:57:54 +0900

    6st commit

--- react/ch5/src/components/Button.jsx
+++ react/ch5/src/components/Button.jsx
@@ -26,6 +26,16 @@ import { version } from "react";
 
 // 객체 구조 분해 할당을 사용한 처리
 // 객체의 프로퍼티 명과 동일하게 매개변수로 정의.
+
+/**
+ * defaultProps 사용이 불가능해질 수 있음.
+ * 따라서, 함수컴포넌트에서 기본값을 설정하는 방법으로
+ * 기본 매개변수 값을 사용하도록 함.
+ *
+ * 기존에는 defaultProps 에 설정된 아래의 내용을
+ * color = "black"
+ * 매개변수 기본값으로 설정 변경함.
+ */
 const Button = ({ text, color = "black", children }) => {
   return (
     <button style={{ color: color }}>
@@ -38,6 +48,7 @@ const Button = ({ text, color = "black", children }) => {
 // props.color.toUpperCase() => 오류 발생 해결 방법.
 // 아래의 코드를 추가한 후,
 // 브라우저에서 새로 고침(F5)해서 확인.
+
 // Button.defaultProps = {
 //   color: "black",
 // };
