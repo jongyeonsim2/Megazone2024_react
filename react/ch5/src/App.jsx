7a03ff9752a4c17e8d7cdb76ded66d760c898af4
unknown <jongyeonsim2@gmail.com>
2024-07-04 10:24:55 +0900

    2st commit

---
+++ react/ch5/src/App.jsx
@@ -0,0 +1,35 @@
+import "./App.css";
+
+function Header() {
+  return (
+    <header>
+      <h1>header</h1>
+    </header>
+  );
+}
+
+function Main() {
+  return (
+    <main>
+      <h1>main</h1>
+    </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <h1>footer</h1>
+    </footer>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <h1>반가웡~~~ 리액트~~~</h1>
+    </>
+  );
+}
+
+export default App;
