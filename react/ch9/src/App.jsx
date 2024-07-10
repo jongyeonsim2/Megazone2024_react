a82115fe3c47488f19337c74f6a31b378c605037
unknown <jongyeonsim2@gmail.com>
2024-07-10 12:11:15 +0900

    2st commit

--- react/ch9/src/App.jsx
+++ react/ch9/src/App.jsx
@@ -1,4 +1,9 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
+import NotFound from "./pages/NotFound";
 
 /***
  * React Router
@@ -7,15 +12,27 @@ import "./App.css";
  *    npm i react-router-dom
  *
  * 2. Router 활용한 컴포넌트 이동
+ *    기본 URL + "라우팅 정보"
+ *    "http://localhost:5173" + "라우팅 정보"
+ *    - "/" : Home 페이지로
+ *    - "/new" : New 페이지로
+ *    - "/diary" : Diary 페이지로
+ *
  * 3. Link 를 이용한 이동
  * 4. 동적 Routing
  */
 
 function App() {
   return (
-    <div>
+    <>
       <h1>App</h1>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary" element={<Diary />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
