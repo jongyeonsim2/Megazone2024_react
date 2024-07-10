1d405dad583da28e5727427a24d65f81b479484e
unknown <jongyeonsim2@gmail.com>
2024-07-10 11:54:30 +0900

    1st commit

---
+++ react/ch9/src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+/***
+ * React Router
+ *
+ * 1. 라이브러리 설치
+ *    npm i react-router-dom
+ *
+ * 2. Router 활용한 컴포넌트 이동
+ * 3. Link 를 이용한 이동
+ * 4. 동적 Routing
+ */
+
+function App() {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/ch9/src/pages/NotFound.jsx
@@ -0,0 +1,5 @@
+const NotFound = () => {
+  return <div>잘못된 페이지임.</div>;
+};
+
+export default NotFound;
