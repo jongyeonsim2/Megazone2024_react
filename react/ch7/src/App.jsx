21a2e6a37927ae7d07afa22ffbbe5ba024631520
unknown <jongyeonsim2@gmail.com>
2024-07-10 09:39:25 +0900

    1st commit

---
+++ react/ch7/src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import Viewer from "./components/Viewer";
+import Controller from "./components/Controller";
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  // event handler
+  const onClickButton = (value) => {
+    setCount(count + value);
+  };
+
+  return (
+    <div>
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer count={count} />
+      </section>
+      <section>
+        <Controller onClickButton={onClickButton} />
+      </section>
+    </div>
+  );
+}
+
+export default App;
