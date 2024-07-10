fa172a96168664cf63c780beac9a87b1c1d28e39
unknown <jongyeonsim2@gmail.com>
2024-07-10 14:34:03 +0900

    5st commit

--- react/ch10/src/App.jsx
+++ react/ch10/src/App.jsx
@@ -3,10 +3,21 @@ import { useRef, useState } from "react";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
+import ExamForUseReducer from "./components/ExamForUseReducer";
 
 /***
- * 기본 데이터 => 상태정보로 관리
- * 글번호, update flag, content, 등록일
+ * - ch08(할 일 관리 앱)의 소스를 사용
+ *
+ * - useReducer hook 활용
+ *   목적 : root app 에는 자식 컴포넌트에서 사용될 기능(함수)을
+ *        직접 구현해서 자식에게 전달하고 있음
+ *
+ *        따라서, 향후에 자식 컴포넌트에서 기능이 대폭 늘어날 수
+ *        있다고 가정하면, root app 관리해야 함
+ *
+ *        그래서, 부모 컴포넌트 함수 내부에 구현해야 함
+ *
+ *        부모 컴포넌트 함수 외부에서 구현하려고 한다면...
  *
  */
 
@@ -37,9 +48,6 @@ function App() {
   // todo 번호, 3번 부터 시작
   const idRef = useRef(3);
 
-  // 새로운 todo 데이터 생성 함수
-  // 1. 데이터 생성
-  // 2. 생성된 데이터를 상태변수에 저장
   const onCreate = (content) => {
     const newToDo = {
       id: idRef.current++,
@@ -50,63 +58,6 @@ function App() {
 
     setTodos([newToDo, ...todos]);
   };
-
-  // 1. C : Create( 등록 )
-  /***
-   * - Editor 컴포넌트의 "추가" 버튼 클릭시
-   *   입력된 todo 정보를
-   *   app component 의 state 정보에 추가
-   *
-   *   - event handler : Editor component
-   *   - app 에서 신규로 생성된 데이터를 전달 : 부모 -> 자식 props
-   *     app component 작성 => 함수로 작성 후 props 로 함수로 전달
-   *   - 글번호 관리
-   *     렌더링과 무관하게 내부적으로 과리해야하는 번호 => useRef hook 사용
-   */
-
-  // 2. 렌더링
-  /***
-   * - root app 컴포넌트에서 관리하는 todo list 데이터를
-   *   List 컴포넌트(자식)로 props 전달
-   * - List 컴포넌트
-   *   todo list 데이터 요소를 TodoItem 컴포넌트에게
-   *   props 전달
-   * - TodoItem 컴포넌트
-   *   todo list 데이터 요소의 값을 html에 표시
-   *
-   * - 중요
-   *   - javascript 배열의 map 함수를 활용
-   *     콜백 함수(배열의 요소에 처리할 내용)를 작성
-   *   - TodoItem 컴포넌트의 input 에 오류 설정
-   *     type="checkbox" 이벤트 핸들러를 구현하지 않으면,
-   *     오류가 발생하는 것에 대한 처리
-   *   - 리스트 형태로 렌더링된 컴포넌트인 경우
-   *     서로를 구분하기 위해 key 가 필요함
-   *     prop 를 통해서 key 전달되록 구현해야 함
-   */
-
-  // 3. R : Read( 검색 )
-  /***
-   * - onChange event handler
-   * - 검색 조건 문자열 -> 상태변수
-   * - 배열의 filter()s 는 콜백함수를 전달
-   *   콜백함수 : includes() 함수를 사용해서 검색 기능 구현
-   */
-
-  // 4. U : Update( 수정 )
-  /***
-   * - 대상 : check box => checked, unchecked
-   *          todo 프로퍼티의 isDone : true, flase
-   * - check box의 onChange event handler
-   * - todo 프로퍼티의 isDone 의 값을 수정하는 기능
-   *   함수로 구현해서 TodoItem(자식 컴포넌트) 에게 전달
-   *   => Root App 에서 관리하는 todo 정보의
-   *      isDone 프로퍼티로 자식 컴포넌트가 접근할 수
-   *      있기 때문임
-   *   => 중요한 것은 데이터의 흐름은 부모에서 자식으로만
-   *      가능하기 때문임
-   *
-   */
 
   // update 함수
   const onUpdate = (targetId) => {
@@ -120,18 +71,6 @@ function App() {
     );
   };
 
-  // 5. D : Delete( 삭제 )
-  /***
-   * - 수정함수처럼 삭제 함수를 root app에서 만든 후
-   *   자식 component 에게 props 로 전달
-   *
-   * - 자식 컴포넌트에서 click envent handler 구현
-   *   부모 컴포넌트가 전달한 삭제 함수를 호출
-   *
-   * - 삭제
-   *   실제 삭제가 아니라, 논리 삭제
-   */
-
   // 삭제 함수
   const onDelete = (targetId) => {
     setTodos(todos.filter((todo) => todo.id !== targetId));
@@ -139,9 +78,7 @@ function App() {
 
   return (
     <div className="App">
-      <Header />
-      <Editor onCreate={onCreate} />
-      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
+      <ExamForUseReducer />
     </div>
   );
 }
---
+++ react/ch10/src/components/ExamForUseReducer.jsx
@@ -0,0 +1,87 @@
+import { useReducer } from "react";
+
+/***
+ * ExamForUseReducer.jsx
+ *
+ *
+ */
+
+/***
+ * 상태를 실제로 변경을 담당하는 함수
+ *
+ * state : 상태값. 변경대상
+ * action : 변경을 원하는 action.
+ *          변경을 희망하는 기준이 되는 요청 정보
+ *          {
+                     type: "increase",
+                    data: 1,
+             }
+ *  
+ */
+function reducer(state, action) {
+  console.log(state, action);
+
+  switch (action.type) {
+    case "increase":
+      return state + action.data;
+    default:
+      return state;
+  }
+}
+
+// 컴포넌트 함수
+const ExamForUseReducer = () => {
+  /***
+   * state : 상태값
+   * dispatch : 상태 변화를 요청하는 정보
+   *
+   * reducer : 개발자가 구현해야 함
+   *           상태를 실제로 변화시키는 기능
+   *
+   *           dispatch 의 요청 정보를 reducer 가 처리
+   *
+   * 0 : state 의 초기값
+   */
+  const [state, dispatch] = useReducer(reducer, 0);
+
+  /***
+   * 1. useReducer() 초기화
+   *    state, dispatch() : 변경요청함수,
+   *    reducer() : 변경실행함수
+   *
+   * 2. button click event
+   *    : 상태값의 변경을 원함
+   *
+   * 3. click event handler 에서
+   *    dispatch() 호출해야 함
+   *
+   *    상태값 변경을 원하니까
+   *
+   *    여기서, 변경을 원하는 정보를 전달
+   *
+   *    dispatch( 변경기준정보 )
+   *
+   * 4. useReducer.reducer 가 자동으로 호출됨
+   * 5. reducer() 에서
+   *    action(변경기준정보) 정보를 바탕으로 수정이 일어남
+   */
+  const onClickPlus = () => {
+    // click event 가 발생해서 변경에 대한 요청을 해야함
+    // 변경을 희망하는 기준정보도 전달
+    // 기준정보를 객체형태로 만들어서 전달
+    dispatch({
+      type: "increase",
+      data: 1,
+    });
+  };
+
+  return (
+    <div>
+      <h1>{state}</h1>
+      <button onClick={onClickPlus}>+</button>
+      <button>-</button>
+    </div>
+  );
+};
+
+export default ExamForUseReducer;
